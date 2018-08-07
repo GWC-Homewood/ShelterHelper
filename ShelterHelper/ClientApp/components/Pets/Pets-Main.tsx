2b27a3685fff2f3bedee0757bc35eae215a9328c
Shane <shane.busby@inspiralsolutions.com>
2018-08-07 16:42:28 -0500

    merge

--- ShelterHelper/ClientApp/components/Pets/Pets-Main.tsx
+++ ShelterHelper/ClientApp/components/Pets/Pets-Main.tsx
@@ -2,11 +2,13 @@
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { ApplicationState } from '../../store';
+import * as PetStore from '../../store/Pets';
+
+
 
 
 export default class PetMain extends React.Component<RouteComponentProps<{}>, {}> {
 
-    
     public render() {
         return <div>
                 
