ae81f4345b08e388d87cd88c42bc0742a184db39
Shane <shane.busby@inspiralsolutions.com>
2018-10-15 00:43:30 -0500

    Setup migrations

--- Pets.Core/Model/ValueObjects/Address.cs
+++ Pets.Core/Model/ValueObjects/Address.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Pets.ValueObjects
 {
+    [Owned]
     public class Address
     {
+        [MaxLength(50)]
         public string StreetAddress1 { get; private set; }
+        [MaxLength(50)]
         public string StreetAddress2 { get; private set; }
+        [MaxLength(50)]
         public string City { get; private set; }
+        [MaxLength(2)]
         public string State { get; private set; }
+        [MaxLength(10)]
         public string Zip { get; private set; }
 
         public Address(string streetAddress1, string streetAddress2, string city, string state, string zip)
--- Pets.Data/PetsContext.cs
+++ Pets.Data/PetsContext.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Pets.Aggregates;
+using Pets.Entities;
 
 namespace Pets.Data
 {
@@ -11,10 +13,18 @@ namespace Pets.Data
             base(options)
         { }
 
+        public DbSet<Pet> Pet { get; set; }
+        public DbSet<Parent> Parent { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasDefaultSchema("Pets");
 
+            modelBuilder.Entity<Pet>().HasKey(p => p.ID);
+            modelBuilder.Entity<Pet>().Property(p => p.Name).HasMaxLength(75);
+            modelBuilder.Entity<Pet>().Property(p => p.MicrochipNumber).HasMaxLength(150);
+
+            modelBuilder.Entity<Parent>().HasKey(p => p.ID);
 
             base.OnModelCreating(modelBuilder);
         }
