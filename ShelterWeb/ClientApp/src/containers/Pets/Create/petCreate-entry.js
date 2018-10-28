dc1f30c04495983a2662d6719bc420e3c0d2bd82
Shane <shane.busby@inspiralsolutions.com>
2018-10-28 01:36:44 -0500

    Pet Create Form

--- ShelterWeb/ClientApp/src/containers/Pets/Create/petCreate-entry.js
+++ ShelterWeb/ClientApp/src/containers/Pets/Create/petCreate-entry.js
@@ -1,11 +1,33 @@
 ﻿import * as React from 'react';
+import { DatePicker } from '@progress/kendo-react-dateinputs';
 
 export const PetCreateEntry = props => (
     <div className='card'>
         <div className='card-header'>Enter a new pet.</div>
         <div className='card-body'>
             <form>
-
+                <div className='form-row'>
+                    <div className='col-sm-4 form-group'>
+                        <label htmlFor='name'>Name</label>
+                        <input className='form-control' name='name' value={props.pet.name} onChange={(e) => props.onChange(e)}/>
+                    </div>
+                </div>
+                <div className='form-row'>
+                    <div className='col-sm-4 form-group'>
+                        <label htmlFor='microchip'>Micro Chip</label>
+                        <input className='form-control' name='microchip' value={props.pet.microchip} onChange={(e) => props.onChange(e)} />
+                    </div>
+                    <div className='col-sm-4 form-group'>
+                        <label htmlFor='birthDate'>Birth Date</label>
+                        <DatePicker className='form-control' name='birthDate' value={props.pet.birthDate} onChange={(e) => props.onChange(e)}/>
+                    </div>
+                </div>
+                <div className='form-row'>
+                    <div className='col-sm-8'>
+                    <button type='button' className='btn btn-outline-primary btn-group'>Create</button>
+                        <button type='button' className='btn btn-outline-secondary' onClick={(e) => { props.goBack(); }}>Cancel</button>
+                    </div>
+                </div>
             </form>
         </div>
     </div>
--- ShelterWeb/ClientApp/src/containers/Pets/Create/petCreate-main.js
+++ ShelterWeb/ClientApp/src/containers/Pets/Create/petCreate-main.js
@@ -4,11 +4,34 @@ import { connect } from 'react-redux';
 import {PetCreateEntry } from '../Create/petCreate-entry';
 
 export class PetCreateMain extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.goBack = this.goBack.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.state = {
+            pet: { id: null, name: '', microchip: '', birthDate: new Date() }
+        };
+    }
+
+    goBack() {
+        this.props.history.go(-1);
+    }
+
+    onChange(e) {
+        let name = e.target.name;
+        let value = e.target.value;
+
+        let newPet = Object.assign({}, this.state.pet);
+        newPet[name] = value;
+        this.setState({ pet: newPet });
+    }
+
     render() {
         return <div>
             <br/>
             <br/>
-            <PetCreateEntry/>
+            <PetCreateEntry goBack={this.goBack} pet={this.state.pet} onChange={this.onChange}/>
         </div>;
     }
 }
